70934128b628510c2448221e1c7a8a0e579139e2
dulei <564779666@qq.com>
2017-03-06 16:40:40 +0800

    fixed bugs

--- routes/table.js
+++ routes/table.js
@@ -3,9 +3,9 @@ var router = express.Router();
 var table = require('../mock/table.json')
 /* GET users listing. */
 router.post('/',  function(req, res, next) {
-  res.json({
-    data:table
-  });
+  res.json(
+    table
+  );
 });
 
 module.exports = router;
