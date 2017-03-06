25ceef9b079cc2f371270a5d6f39b86de93522c6
dulei <564779666@qq.com>
2017-03-06 16:09:03 +0800

    change get to post

--- routes/table.js
+++ routes/table.js
@@ -2,9 +2,8 @@ var express = require('express');
 var router = express.Router();
 var table = require('../mock/table.json')
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.post('/',  function(req, res, next) {
   res.json({
-    errNo: 1,
     data:table
   });
 });
