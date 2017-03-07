b96d26cbd578d6faef1879ffee3d95ac48a37470
dulei <564779666@qq.com>
2017-03-07 15:20:03 +0800

    change param

--- routes/table.js
+++ routes/table.js
@@ -5,7 +5,7 @@ var table = require('../mock/table.json')
 /* GET users listing. */
 router.post('/',  function(req, res, next) {
   console.log(req.body)
-  if(req.body.pageLen==5){
+  if(req.body.len==5){
     const lists = table.lists.slice(1,5);
     table.lists = lists
     res.json(
