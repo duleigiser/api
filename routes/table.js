e7c42b8fe06ba2ea916778f8c62017af57dc01ae
dulei <564779666@qq.com>
2017-03-07 14:56:23 +0800

    增加请求参数处理

--- routes/table.js
+++ routes/table.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var router = express.Router();
 var table = require('../mock/table.json')
+
 /* GET users listing. */
 router.post('/',  function(req, res, next) {
-  res.json(
-    table
-  );
+  console.log(req.body)
+  if(req.body.pageLen==5){
+    const lists = table.lists.slice(1,5);
+    table.lists = lists
+    res.json(
+      table
+    )
+  }else{
+    res.json(
+      table
+    )
+  }
+  
 });
 
 module.exports = router;
